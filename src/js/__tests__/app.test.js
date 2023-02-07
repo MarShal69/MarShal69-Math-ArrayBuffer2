929816ab937585622fc833bc1dd316f6c86d23c5
Marina Shalamova <tusya120@mail.ru>
2023-02-07 18:49:27 +0300

    first commit

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,11 @@
+// import ArrayBufferConverter from '../app';
+// import getBuffer from '../initialData';
+
+// test('загрузка данных', () => {
+//     const buffer = new ArrayBufferConverter();
+//     const result = buffer.load();
+//     const expected = [];
+
+//     expect(result).toBe(expected); 
+
+// })
---
+++ src/js/app.js
@@ -0,0 +1,27 @@
+import getBuffer from './initialData';
+
+export default class ArrayBufferConverter {
+// class ArrayBufferConverter {
+  // eslint-disable-next-line class-methods-use-this
+  load() {
+    const data = new Uint16Array(getBuffer());
+    return data;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  toString(data) {
+    console.log(data);
+    const data1 = [];
+    data.forEach((el) => {
+      data1.push(String.fromCharCode(el));
+    });
+    const dataStr = data1.join('');
+    return dataStr;
+  }
+}
+
+
+const buffer = new ArrayBufferConverter();
+console.log(buffer);
+console.log(buffer.load());
+console.log(buffer.toString(buffer.load()));
